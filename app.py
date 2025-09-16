632a8c885bab63d98f2be02af0a3470d890253e2
DollarDonut <benlassinadiaby@gmail.com>
2025-09-16 19:16:54 +0200

    alle drivers gepakt van de FastF1 API

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 import fastf1
+import fastf1.plotting
 
-session = fastf1.get_session(2023, 'Australian Grand Prix', 'Q')
+fastf1.Cache.enable_cache("cache")
+
+session = fastf1.get_session(2025, 'Dutch Grand Prix', 'R')
 session.load(telemetry=False, laps=True, weather=True)
 
-pia = session.get_driver('PIA') 
+pia = session.get_driver('VER') 
 print(f"Pronto {pia['FirstName']}?")
+
+
+for drv in session.drivers: 
+    driver = session.get_driver(drv)
+    print(drv, driver['FullName'])
